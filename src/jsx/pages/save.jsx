c38d75ea673044b3899fa33b3c56c9417ca5a4a2
38elements <mh19820223@gmail.com>
2018-12-08 02:04:26 +0900

    feat: improve save page

--- src/jsx/pages/save.jsx
+++ src/jsx/pages/save.jsx
@@ -3,16 +3,47 @@ import { connect } from 'unistore/preact'
 
 import actions from '../actions/save.jsx'
 import Header from '../parts/header.jsx'
+import Navigator from '../parts/navigator.jsx'
 
 class Save extends Component {
-  render({ saved }, { url }) {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+    }
+    this.setTitle = this.setTitle.bind(this)
+    this.save = this.save.bind(this)
+  }
+  setTitle(e) {
+    this.setState({ title: e.target.value })
+  }
+  save() {
+    if (this.state.title) {
+      this.props.save(this.state.title)
+    }
+  }
+  render({ edit, save, saved }) {
+    let content
+    if (saved) {
+      content = <p>The code and comment was saved.</p>
+    }
+    else {
+      content = (
+        <div>
+          Title: <input type="text" className="title" onChange={ this.setTitle } />
+          <button onClick={ this.save }>Save</button>
+        </div>
+      )
+    }
     return (
       <div className="cc-save center">
         <Header />
-        <div>
-          If you want to save the code and comment,<br/>
-          Please input the title for code and comment.
-        </div>
+        <Navigator
+          leftLabel={ '<-Edit' }
+          leftClick={ edit }
+        />
+        <p>If you want to save the code and comment, Please input the title for code and comment.</p>
+        { content }
       </div>
     )
   }
