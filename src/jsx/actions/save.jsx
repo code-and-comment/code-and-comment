a21e822b16eea193e683d604330071a59a7a7f6c
38elements <mh19820223@gmail.com>
2018-12-08 23:03:15 +0900

    feat: add save function

--- src/jsx/actions/save.jsx
+++ src/jsx/actions/save.jsx
@@ -1,13 +1,27 @@
 import { route as _route } from 'preact-router'
 
+import { getDB, getObjectStore, addRecord } from '../db.jsx'
+
 
 const actions = () => ({
   edit(state, event, route = _route) {
     route('/edit')
   },
-  save(state, title) {
+  async save(state, title) {
     title = title.trim()
     if (title) {
+      const db = await getDB()
+      const objectStore = await getObjectStore(db)
+      const parts = state.path.split('/')
+      // TODO add error process
+      await addRecord(objectStore, {
+        title,
+        git: state.git,
+        path: state.path,
+        lines: state.lines,
+        comments: state.comments,
+        repository: `${parts[1]}/${parts[2]}`,
+      })
       return { saved: true }
     }
   }
