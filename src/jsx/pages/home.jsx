db8c205a620c924115fb50092a5c2a5088585c70
38elements <38elements@users.noreply.github.com>
2018-12-19 21:49:36 +0900

    feat: add actions/home.jsx (#93)

--- src/jsx/pages/home.jsx
+++ src/jsx/pages/home.jsx
@@ -1,25 +1,30 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 import { Link } from 'preact-router'
+import { connect } from 'unistore/preact'
 
+import actions from '../actions/home.jsx'
 import Header from '../parts/header.jsx'
 
-function Home() {
-  return (
-    <div className="cc-home center">
-      <Header />
-      <div>
-        The status of this application is pre-alpha.<br/>
-        Many breaking changes will be added to this application.<br/>
-        This is demo site.<br/>
-        If you want to use this application as product, you should host this application.<br/>
+
+class Home extends Component {
+  render({ search_code_and_comment }) {
+    return (
+      <div className="cc-home center">
+        <Header />
+        <div>
+          The status of this application is pre-alpha.<br/>
+          Many breaking changes will be added to this application.<br/>
+          This is demo site.<br/>
+          If you want to use this application as product, you should host this application.<br/>
+        </div>
+        <div className="links">
+          <p><Link href="/start">Create Code and Comment</Link></p>
+          <p><a onClick={ search_code_and_comment }>Code and Comment List</a></p>
+        </div>
       </div>
-      <div className="links">
-        <p><Link href="/start">Create Code and Comment</Link></p>
-        <p><Link href="/search_code_and_comment">Code and Comment List</Link></p>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 
-export default Home
+export default connect(null, actions)(Home)
