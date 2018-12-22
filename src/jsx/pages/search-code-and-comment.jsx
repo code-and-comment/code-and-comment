5ddd4af6935eb139412c94bb85c0950b6033b885
38elements <38elements@users.noreply.github.com>
2018-12-22 12:28:20 +0900

    feat: add delete function (#96)

--- src/jsx/pages/search-code-and-comment.jsx
+++ src/jsx/pages/search-code-and-comment.jsx
@@ -3,17 +3,33 @@ import { connect } from 'unistore/preact'
 
 import actions from '../actions/search-code-and-comment.jsx'
 import Header from '../parts/header.jsx'
+import Navigator from '../parts/navigator.jsx'
+import CodeAndCommentCard from '../parts/code-and-comment-card.jsx'
+
 
 class SearchCodeAndComment extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
-  render({ codeAndComments }) {
+  render({ codeAndComments, deleteOne, edit, home }) {
+    const length = codeAndComments.length - 1
+    const list =  codeAndComments.map((c, i) => {
+      if (length == i) {
+        return <CodeAndCommentCard key={ c.id } codeAndComment={ c } edit={ edit } deleteOne={ deleteOne } />
+      }
+      return [<CodeAndCommentCard key={ c.id } codeAndComment={ c } edit={ edit } deleteOne={ deleteOne } />, <hr/>]
+    })
     return (
       <div className="cc-search-code-and-comment center">
         <Header />
-        SearchCodeAndComment
+        <Navigator
+          leftLabel={ '<-Home' }
+          leftClick={ home }
+        />
+        <div className="list">
+          { list }
+        </div>
       </div>
     )
   }
