1c99f599e10afaf05583a0618a76965fd2c177d0
38elements <mh19820223@gmail.com>
2018-12-24 21:52:28 +0900

    feat: add src/jsx/pages/search-comment.jsx

--- src/jsx/actions/home.jsx
+++ src/jsx/actions/home.jsx
@@ -1,16 +1,24 @@
 import { route as _route } from 'preact-router'
 
-import { getDB, getObjectStore, getAllRecords } from '../db.jsx'
+import { getDB as _getDB, getObjectStore as _getObjectStore, getAllRecords as _getAllRecords } from '../db.jsx'
+
+
+export async function _transfer(path, route = _route, getDB = _getDB, getObjectStore = _getObjectStore, getAllRecords = _getAllRecords) {
+  route(path)
+  const db = await getDB()
+  const objectStore = await getObjectStore(db)
+  const codeAndComments = await getAllRecords(objectStore)
+  return { codeAndComments }
+}
 
 
 const actions = () => ({
-  async search_code_and_comment(state, event, route = _route) {
-    route('/search_code_and_comment')
-    const db = await getDB()
-    const objectStore = await getObjectStore(db)
-    const codeAndComments = await getAllRecords(objectStore) 
-    return { codeAndComments }
-  }
+  search_code_and_comment(state, event, transfer = _transfer) {
+    return transfer('/search_code_and_comment')
+  },
+  search_comment(state, event, transfer = _transfer) {
+    return transfer('/search_comment')
+  },
 })
 
 
