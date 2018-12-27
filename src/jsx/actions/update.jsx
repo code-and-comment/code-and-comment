c7d40405d971e1e5a8030c7fdffb08f2038f9950
38elements <38elements@users.noreply.github.com>
2018-12-27 23:56:33 +0900

    feat: save viewUrl (#118)

--- src/jsx/actions/update.jsx
+++ src/jsx/actions/update.jsx
@@ -1,6 +1,7 @@
 import { route as _route } from 'preact-router'
 
 import { getDB as _getDB, getObjectStore as _getObjectStore, putRecord as _putRecord } from '../db.jsx'
+import { createViewUrl } from '../utils.jsx'
 
 
 const actions = () => ({
@@ -13,6 +14,7 @@ const actions = () => ({
       const db = await getDB()
       const objectStore = await getObjectStore(db)
       const parts = state.path.split('/')
+      const viewUrl = createViewUrl(state.git, state.path, state.comments)
       // TODO add error process
       await putRecord(objectStore, {
         id: state.id,
@@ -22,6 +24,7 @@ const actions = () => ({
         lines: state.lines,
         comments: state.comments,
         repository: `${parts[1]}/${parts[2]}`,
+        viewUrl
       })
       return {
         title,
