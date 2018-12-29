242bbaf22d95c83ba31a516433e5b02594b71b6f
38elements <38elements@users.noreply.github.com>
2018-12-29 21:27:41 +0900

    feat: search comment (#130)

--- src/jsx/pages/search-comment.jsx
+++ src/jsx/pages/search-comment.jsx
@@ -7,10 +7,54 @@ import Navigator from '../parts/navigator.jsx'
 import CommentCard from '../parts/comment-card.jsx'
 
 
-function CommentList({ codeAndComments, edit }) {
+class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      repository: '',
+      comment: ''
+    }
+    this.search = this.search.bind(this)
+    this.setRepository = this.setRepository.bind(this)
+    this.setComment = this.setComment.bind(this)
+  }
+  setRepository(event) {
+    this.setState({
+      repository: event.target.value
+    })
+  }
+  setComment(event) {
+    this.setState({
+      comment: event.target.value
+    })
+  }
+  search() {
+    const { repository, comment } = this.state
+    this.props.search(repository, comment)
+  }
+  render(_, { repository, comment }) {
+    return (
+      <div className="search">
+        <div>Repository: <input type="text" value={ repository } onChange={ this.setRepository } /></div>
+        <div>Comment: <input type="text" value={ comment } onChange={ this.setComment } /></div>
+        <div><button onClick={ this.search } >Search</button></div>
+      </div>
+    )
+  }
+}
+
+
+function CommentList({ codeAndComments, commentPattern, edit }) {
   const list = []
+  let re
+  if (commentPattern) {
+    re = new RegExp(commentPattern)
+  }
   codeAndComments.forEach((c) => {
     Object.values(c.comments).forEach((comment, i) => {
+      if (re && !comment.match(re)) {
+        return
+      }
       list.push(<CommentCard key={ `${c.id}_${i}` } comment={ comment } codeAndComment={ c } edit={ edit } />)
       list.push(<hr />)
     })
@@ -25,12 +69,26 @@ function CommentList({ codeAndComments, edit }) {
 
 
 class SearchComment extends Component {
-  render({ codeAndComments, edit, home }) {
+  constructor(props) {
+    super(props)
+    this.state = {
+      comment: ''
+    }
+    this.search = this.search.bind(this)
+  }
+  search(repository, comment) {
+    this.setState({
+      comment
+    })
+    this.props.search({ repository })
+  }
+  render({ codeAndComments, edit, home }, { comment }) {
     return (
       <div className="cc-search-comment center">
         <Header />
         <Navigator leftLabel={ '<-Home' } leftClick={ home } />
-        <CommentList codeAndComments={ codeAndComments } edit={ edit } />
+        <Search search= { this.search } />
+        <CommentList codeAndComments={ codeAndComments } commentPattern={ comment } edit={ edit } />
       </div>
     )
   }
