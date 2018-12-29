80d8bf7de1c9af417eb6dedae112886a4736a45d
38elements <mh19820223@gmail.com>
2018-12-29 12:09:42 +0900

    refactor: list

--- src/jsx/pages/search-comment.jsx
+++ src/jsx/pages/search-comment.jsx
@@ -7,26 +7,30 @@ import Navigator from '../parts/navigator.jsx'
 import CommentCard from '../parts/comment-card.jsx'
 
 
+function CommentList({ codeAndComments, edit }) {
+  const list = []
+  codeAndComments.forEach((c) => {
+    Object.values(c.comments).forEach((comment, i) => {
+      list.push(<CommentCard key={ `${c.id}_${i}` } comment={ comment } codeAndComment={ c } edit={ edit } />)
+      list.push(<hr />)
+    })
+  })
+  list.pop()
+  return (
+    <div className="comment-list">
+      { list.length ? list : 'There is no comment.' }
+    </div>
+  )
+}
+
+
 class SearchComment extends Component {
   render({ codeAndComments, edit, home }) {
-    const list = []
-    codeAndComments.forEach((c) => {
-      Object.values(c.comments).forEach((comment, i) => {
-        list.push(<CommentCard key={ `${c.id}_${i}` } comment={ comment } codeAndComment={ c } edit={ edit } />)
-        list.push(<hr />)
-      })
-    })
-    list.pop()
     return (
       <div className="cc-search-comment center">
         <Header />
-        <Navigator
-          leftLabel={ '<-Home' }
-          leftClick={ home }
-        />
-        <div className="list">
-          { list.length ? list : 'There is no comment.' }
-        </div>
+        <Navigator leftLabel={ '<-Home' } leftClick={ home } />
+        <CommentList codeAndComments={ codeAndComments } edit={ edit } />
       </div>
     )
   }
