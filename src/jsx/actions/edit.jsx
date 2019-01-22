5f1e13a208482117e6dccd2df19068fde1de2fbd
38elements <38elements@users.noreply.github.com>
2019-01-23 00:02:04 +0900

    refactor: actions (#219)

--- src/jsx/actions/edit.jsx
+++ src/jsx/actions/edit.jsx
@@ -15,92 +15,110 @@ import {
 } from '../utils.jsx'
 
 
-const actions = () => ({
-  async updateComment(
-    state,
-    index,
-    comment,
-    updateCodeAndComment = _updateCodeAndComment,
-    getDB = _getDB,
-    getObjectStore = _getObjectStore,
-    putRecord = _putRecord
-  ) {
-    let comments = state.comments
-    comment = comment.trim()
-    index += ''
-    if (comments[index] === comment) {
-      return
-    }
-    else if (comment === '') {
-      if (comments[index]) {
-        delete comments[index]
-      }
-      else {
-        return
-      }
+async function updateComment(
+  state,
+  index,
+  comment,
+  updateCodeAndComment = _updateCodeAndComment,
+  getDB = _getDB,
+  getObjectStore = _getObjectStore,
+  putRecord = _putRecord
+) {
+  let comments = state.comments
+  comment = comment.trim()
+  index += ''
+  if (comments[index] === comment) {
+    return
+  }
+  else if (comment === '') {
+    if (comments[index]) {
+      delete comments[index]
     }
     else {
-      comments[index] = comment
+      return
     }
-    comments = Object.assign({}, comments)
+  }
+  else {
+    comments[index] = comment
+  }
+  comments = Object.assign({}, comments)
+  await updateCodeAndComment(
+    Object.assign(state, { comments }),
+    getDB,
+    getObjectStore,
+    putRecord
+  )
+  return { comments }
+}
+
+
+async function updateTitle(
+  state,
+  event,
+  updateCodeAndComment = _updateCodeAndComment,
+  getDB = _getDB,
+  getObjectStore = _getObjectStore,
+  putRecord = _putRecord
+) {
+  const title = event.target.value.trim()
+  if (title) {
     await updateCodeAndComment(
-      Object.assign(state, { comments }),
+      Object.assign(state, { title }),
       getDB,
       getObjectStore,
       putRecord
     )
-    return { comments }
-  },
-  async updateTitle(
-    state,
-    event,
-    updateCodeAndComment = _updateCodeAndComment,
-    getDB = _getDB,
-    getObjectStore = _getObjectStore,
-    putRecord = _putRecord
-  ) {
-    const title = event.target.value.trim()
-    if (title) {
-      await updateCodeAndComment(
-        Object.assign(state, { title }),
-        getDB,
-        getObjectStore,
-        putRecord
-      )
-      return { title }
-    }
-  },
-  fileUrl(state, event, route = _route) {
-    route('/start')
-  },
-  list(
-    state,
-    event,
-    route = _route,
-    getDB = _getDB,
-    getObjectStore = _getObjectStore,
-    getAllRecords = _getAllRecords
-  ) {
-    return transfer('/search_code_and_comment', route, getDB, getObjectStore, getAllRecords)
-  },
-  publish(state, event, route = _route, location = window.location) {
-    const viewUrl = createViewUrl(state.git, state.path, state.comments, location)
-    route('/publish')
-    return { viewUrl }
-  },
-  deleteOne(
-    state,
-    id,
-    event,
-    route = _route,
-    getDB = _getDB,
-    getObjectStore = _getObjectStore,
-    deleteRecord = _deleteRecord,
-    getAllRecords = _getAllRecords
-  ) {
-    _deleteOne(id, getDB, getObjectStore, deleteRecord)
-    return transfer('/search_code_and_comment', route, getDB, getObjectStore, getAllRecords)
-  },
+    return { title }
+  }
+}
+
+
+function fileUrl(state, event, route = _route) {
+  route('/start')
+}
+
+
+function list(
+  state,
+  event,
+  route = _route,
+  getDB = _getDB,
+  getObjectStore = _getObjectStore,
+  getAllRecords = _getAllRecords
+) {
+  return transfer('/search_code_and_comment', route, getDB, getObjectStore, getAllRecords)
+}
+
+
+function publish(state, event, route = _route, location = window.location) {
+  const viewUrl = createViewUrl(state.git, state.path, state.comments, location)
+  route('/publish')
+  return { viewUrl }
+}
+
+
+function deleteOne(
+  state,
+  id,
+  event,
+  route = _route,
+  getDB = _getDB,
+  getObjectStore = _getObjectStore,
+  deleteRecord = _deleteRecord,
+  getAllRecords = _getAllRecords
+) {
+  _deleteOne(id, getDB, getObjectStore, deleteRecord)
+  return transfer('/search_code_and_comment', route, getDB, getObjectStore, getAllRecords)
+}
+
+
+const actions = () => ({
+  updateComment,
+  updateTitle,
+  fileUrl,
+  list,
+  publish,
+  deleteOne,
 })
 
 
--- src/jsx/actions/start.jsx
+++ src/jsx/actions/start.jsx
@@ -5,76 +5,88 @@ import { getDB as _getDB, getObjectStore as _getObjectStore, addRecord as _addRe
 import { saveCodeAndComment as _saveCodeAndComment } from '../utils.jsx'
 
 
-const actions = () => ({
-  back(state, event, route = _route) {
-    route('/edit')
-  },
-  home(state, event, route = _route) {
-    route('/home')
-  },
-  setLoading(state, loading) {
-    return { loading }
-  },
-  async getFile(
-    state,
-    url,
-    route = _route,
-    fetch = window.fetch,
-    saveCodeAndComment = _saveCodeAndComment,
-    getDB = _getDB,
-    getObjectStore = _getObjectStore,
-    addRecord = _addRecord
-  ) {
-    url = url.trim()
-    const re = /^https:\/\/github.com\/(.+)\/blob\/([^/]+)\/(.+)/
-    const matches = url.match(re)
+function back(state, event, route = _route) {
+  route('/edit')
+}
 
-    if (!matches) {
-      return {
-        loading: false,
-        networkError: false,
-        urlError: true
-      }
-    }
 
-    const apiUrl = `https://api.github.com/repos/${matches[1]}/contents/${matches[3]}?ref=${matches[2]}`
+function home(state, event, route = _route) {
+  route('/home')
+}
 
-    const data = await fetch(apiUrl)
-      .then(response => {
-        if (response.ok) {
-          return response.json()
-        }
-        return null
-      })
 
-    if (data && data.type === 'file') {
-      route('/edit')
-      const git = data._links.git
-      const path = url.substring(18)
-      const lines = Base64.decode(data.content).split('\n')
-      const comments = {}
-      const title = 'New Code and Comment'
-      const state = { title, git, path, lines, comments }
-      const id = await saveCodeAndComment(state, getDB, getObjectStore, addRecord)
-      return {
-        id,
-        loading: false,
-        title,
-        git,
-        path,
-        lines,
-        comments,
-        networkError: false,
-        urlError: false
-      }
-    }
+function setLoading(state, loading) {
+  return { loading }
+}
 
+
+async function getFile(
+  state,
+  url,
+  route = _route,
+  fetch = window.fetch,
+  saveCodeAndComment = _saveCodeAndComment,
+  getDB = _getDB,
+  getObjectStore = _getObjectStore,
+  addRecord = _addRecord
+) {
+  url = url.trim()
+  const re = /^https:\/\/github.com\/(.+)\/blob\/([^/]+)\/(.+)/
+  const matches = url.match(re)
+
+  if (!matches) {
     return {
       loading: false,
-      networkError: true,
+      networkError: false,
+      urlError: true
+    }
+  }
+
+  const apiUrl = `https://api.github.com/repos/${matches[1]}/contents/${matches[3]}?ref=${matches[2]}`
+
+  const data = await fetch(apiUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      return null
+    })
+
+  if (data && data.type === 'file') {
+    route('/edit')
+    const git = data._links.git
+    const path = url.substring(18)
+    const lines = Base64.decode(data.content).split('\n')
+    const comments = {}
+    const title = 'New Code and Comment'
+    const state = { title, git, path, lines, comments }
+    const id = await saveCodeAndComment(state, getDB, getObjectStore, addRecord)
+    return {
+      id,
+      loading: false,
+      title,
+      git,
+      path,
+      lines,
+      comments,
+      networkError: false,
       urlError: false
     }
   }
+
+  return {
+    loading: false,
+    networkError: true,
+    urlError: false
+  }
+}
+
+
+const actions = () => ({
+  back,
+  home,
+  setLoading,
+  getFile,
 })
 
 
