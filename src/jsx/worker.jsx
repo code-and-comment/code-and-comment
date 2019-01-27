ce324b7a0175c31a4d055fe6e99f103236ac9195
38elements <38elements@users.noreply.github.com>
2019-01-27 23:32:50 +0900

    chore: dist/repositories.min.js (#235)

--- src/jsx/worker.jsx
+++ src/jsx/worker.jsx
@@ -2,7 +2,7 @@ let repositoriesWoker
 
 export function updateRepositories(store) {
   if (!repositoriesWoker) {
-    repositoriesWoker = new Worker('dist/repositories.js')
+    repositoriesWoker = new Worker('dist/repositories.min.js')
     repositoriesWoker.addEventListener('message', (event) => {
       store.setState({ repositories: event.data })
     }, false)
