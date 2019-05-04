55b285ec32da417b67ab9553758ec897a1b87e35
38elements <38elements@users.noreply.github.com>
2019-05-04 19:44:51 +0900

    chore: webpack.config.js (#453)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'development',
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 1000,
+    poll: 1500,
+    ignored: ['node_modules', 'dist']
+  },
   entry: {
     app: './src/jsx/index.jsx',
     repositories: './src/jsx/repositories.jsx',
     codeAndComments: './src/jsx/codeAndComments.jsx',
+    index: './src/scss/index.scss',
   },
   devtool: 'source-map',
   module: {
@@ -23,12 +32,25 @@ module.exports = {
         }
       },
       {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ] 
+      },
+      {
         test: /(\.mjs$|\.js$)/,
         use: ['source-map-loader'],
         enforce: 'pre'
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
+    }),
+  ],
   output: {
     filename: '[name].js',
     path: __dirname + '/dist'
