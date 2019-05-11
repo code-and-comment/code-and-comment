2518d4303fc1d21ac4ae854c478f482e809262dc
38elements <38elements@users.noreply.github.com>
2019-05-11 23:38:16 +0900

    test: enzyme-adapter-preact-pure (#465)

--- test/parts/test_header.js
+++ test/parts/test_header.js
@@ -1,12 +1,14 @@
 import { h } from 'preact'
+import { mount } from 'enzyme'
 import { expect } from 'chai'
 
 import Header from '../../src/jsx/parts/header.jsx'
 
 describe('parts/header', () => {
-  describe('Header', () => {
-    it('returns vnodes ', () => {
-      expect(Header()).to.deep.equal(<div className="cc-header"><h1>Code and Comment</h1></div>)
+  describe('<Header />', () => {
+    it('is a header', () => {
+      const wrapper = mount(<Header />)
+      expect(wrapper.html()).to.equal('<div class="cc-header"><h1>Code and Comment</h1></div>')
     })
   })
 })
---
+++ test/setup.js
@@ -0,0 +1,6 @@
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-preact-pure'
+
+
+const adapter = new Adapter()
+configure({ adapter })
