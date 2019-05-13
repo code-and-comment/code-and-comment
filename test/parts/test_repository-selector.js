859dbabc5be7c69655b8d2454fac9083f5df04a8
38elements <mh19820223@gmail.com>
2019-05-13 23:58:56 +0900

    test: repository-selector

--- test/parts/test_repository-selector.js
+++ test/parts/test_repository-selector.js
@@ -8,9 +8,13 @@ describe('parts/repository-selector', () => {
   describe('<RepositorySelector />', () => {
     it('displays repositories', () => {
       const searchRepository = function() {}
-      const wrapper = mount(<RepositorySelector repositories={ ['a', 'b'] } searchRepository={ searchRepository } />)
-      const html = '<div class="cc-repository-selector"><div class="application-name">Code and Comment</div>'
+      let wrapper = mount(<RepositorySelector repositories={ ['a', 'b'] } searchRepository={ searchRepository } />)
+      let html = '<div class="cc-repository-selector"><div class="application-name">Code and Comment</div>'
       + '<div class="repository" data-repository="a">a</div><div class="repository" data-repository="b">b</div></div>'
+      expect(wrapper.html()).to.equal(html)
+
+      wrapper = mount(<RepositorySelector repositories={ [] } searchRepository={ searchRepository } />)
+      html = '<div class="cc-repository-selector"><div class="application-name">Code and Comment</div></div>'
       expect(wrapper.html()).to.equal(html)
     })
   })
