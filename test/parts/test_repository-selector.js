2e53e7d69685349ea0379ae397a48eb757dc4fed
38elements <mh19820223@gmail.com>
2019-05-15 00:07:18 +0900

    test: repository-selector

--- test/parts/test_repository-selector.js
+++ test/parts/test_repository-selector.js
@@ -1,4 +1,5 @@
 import { h } from 'preact'
+import { spy } from 'sinon'
 import { mount } from 'enzyme'
 import { expect } from 'chai'
 
@@ -7,15 +8,23 @@ import { RepositorySelector } from '../../src/jsx/parts/repository-selector.jsx'
 describe('parts/repository-selector', () => {
   describe('<RepositorySelector />', () => {
     it('displays repositories', () => {
-      const searchRepository = function() {}
-      let wrapper = mount(<RepositorySelector repositories={ ['a', 'b'] } searchRepository={ searchRepository } />)
+      const setCodeAndComments = function() {}
+      let wrapper = mount(<RepositorySelector repositories={ ['a', 'b'] } searchRepository={ 'b' } setCodeAndComments={ setCodeAndComments } />)
       let html = '<div class="cc-repository-selector"><div class="application-name">Code and Comment</div>'
-      + '<div class="repository" data-repository="a">a</div><div class="repository" data-repository="b">b</div></div>'
+      + '<div class="repository" data-repository="a">a</div><div class="repository selected" data-repository="b">b</div></div>'
       expect(wrapper.html()).to.equal(html)
 
-      wrapper = mount(<RepositorySelector repositories={ [] } searchRepository={ searchRepository } />)
+      wrapper = mount(<RepositorySelector repositories={ [] } searchRepository={ 'b' } setCodeAndComments={ setCodeAndComments } />)
       html = '<div class="cc-repository-selector"><div class="application-name">Code and Comment</div></div>'
       expect(wrapper.html()).to.equal(html)
+    })
+
+    it('selects a repository', () => {
+      const setCodeAndComments = spy()
+      const wrapper = mount(<RepositorySelector repositories={ ['a', 'b'] } searchRepository={ 'b' } setCodeAndComments={ setCodeAndComments } />)
+      const repository = wrapper.find('.repository').at(1)
+      repository.simulate('click')
+      expect(setCodeAndComments.args[0][0]).to.equal('b')
     })
   })
 })
