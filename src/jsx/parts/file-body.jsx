c36555f8f9d5b02075a948001797ebd558aaee57
38elements <mh19820223@gmail.com>
2019-06-16 13:56:35 +0900

    feat: keyboard shortcuts

--- src/jsx/parts/file-body.jsx
+++ src/jsx/parts/file-body.jsx
@@ -1,36 +1,68 @@
-import { h } from 'preact'
+import { h, Component } from 'preact'
 
 import Line from './line.jsx'
+import { scrollIntoView } from '../utils.jsx'
 
 
-function FileBody({
-  id,
-  lines,
-  comments,
-  updateComment,
-  editable,
-  hiddenSignal,
-  highlightLineNumber,
-}) {
-  return (
-    <div className="cc-file-body">
-      { lines.map((code, index) => {
-        return (<Line
-          id={ id }
-          key={ index }
-          code={ code }
-          comment={ comments[index + ''] }
-          index={ index }
-          updateComment={ updateComment }
-          editable={ editable }
-          hiddenSignal={ hiddenSignal }
-          isHighlight={ highlightLineNumber === (index + 1) }
-        />)
-      }
-      ) }
-    </div>
-  )
+class FileBody extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markedLineNumber: 0
+    }
+    this.setMarkedLineNumber = this.setMarkedLineNumber.bind(this)
+    this.scrollToMarkedLineNumber = this.scrollToMarkedLineNumber.bind(this)
+  }
+
+  componentWillReceiveProps({ id }) {
+    if (this.props.id !== id) {
+      this.setState({ markedLineNumber: 0 })
+    }
+  }
+
+  setMarkedLineNumber(event) {
+    event.stopPropagation()
+    const markedLineNumber = event.currentTarget.dataset.lineNumber - 0
+    this.setState({ markedLineNumber })
+  }
+
+  scrollToMarkedLineNumber() {
+    scrollIntoView(this.state.markedLineNumber)
+  }
+
+  render({
+    id,
+    lines,
+    comments,
+    updateComment,
+    editable,
+    hiddenSignal,
+    highlightLineNumber,
+  }, {
+    markedLineNumber
+  }) {
+    return (
+      <div className="cc-file-body">
+        { lines.map((code, index) => {
+          return (<Line
+            id={ id }
+            key={ index }
+            code={ code }
+            comment={ comments[index + ''] }
+            index={ index }
+            updateComment={ updateComment }
+            editable={ editable }
+            hiddenSignal={ hiddenSignal }
+            isHighlight={ highlightLineNumber === (index + 1) }
+            isMarked={ markedLineNumber === (index + 1) }
+            setMarkedLineNumber={ this.setMarkedLineNumber }
+            scrollToMarkedLineNumber={ this.scrollToMarkedLineNumber }
+          />)
+        }
+        ) }
+      </div>
+    )
+  }
 }
-
 
 export default FileBody
