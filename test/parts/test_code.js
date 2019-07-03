16062b040262b5708acaedf9b0b468244d36a717
38elements <mh19820223@gmail.com>
2019-07-04 00:43:58 +0900

    test: Code

--- test/parts/test_code.js
+++ test/parts/test_code.js
@@ -32,5 +32,19 @@ describe('parts/code', () => {
       const wrapper = mount(<Code isHidden={ true } />)
       expect(wrapper.exists('.number.hidden')).to.be.true
     })
+
+    it('executes scrollToMarkedLineNumber()', () => {
+      const scrollToMarkedLineNumber = spy()
+      const wrapper = mount(<Code scrollToMarkedLineNumber={ scrollToMarkedLineNumber } />)
+      wrapper.find('.cc-code').at(0).simulate('keydown', { keyCode: 71 })
+      expect(scrollToMarkedLineNumber.calledOnce).to.be.true
+    })
+
+    it('executes setMarkedLineNumber()', () => {
+      const setMarkedLineNumber = spy()
+      const wrapper = mount(<Code setMarkedLineNumber={ setMarkedLineNumber } />)
+      wrapper.find('.cc-code').at(0).simulate('keydown', { keyCode: 77 })
+      expect(setMarkedLineNumber.calledOnce).to.be.true
+    })
   })
 })
