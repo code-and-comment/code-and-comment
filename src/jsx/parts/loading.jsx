fa43f511174603f8c0b47672e4627815fcc8f95f
38elements <38elements@users.noreply.github.com>
2019-09-18 22:26:04 +0900

    type: src/jsx/parts/loading.tsx (#558)

--- src/jsx/parts/loading.jsx
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact'
-
-
-function Loading() {
-  return <div className="cc-loading"></div>
-}
-
-export default Loading
---
+++ src/jsx/parts/loading.tsx
@@ -0,0 +1,8 @@
+import { h, FunctionalComponent } from 'preact'
+
+
+const Loading: FunctionalComponent = function Loading() {
+  return <div className="cc-loading"></div>
+}
+
+export default Loading
