b595ae7021b220b62176c2aee58fe63480a45a51
38elements <mh19820223@gmail.com>
2019-09-19 22:24:45 +0900

    type: header.tsx

--- src/jsx/parts/header.jsx
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact'
-
-
-function Header() {
-  return <div className="cc-header"><h1>Code and Comment</h1></div>
-}
-
-export default Header
---
+++ src/jsx/parts/header.tsx
@@ -0,0 +1,8 @@
+import { h, FunctionalComponent } from 'preact'
+
+
+const Header: FunctionalComponent = function Header() {
+  return <div className="cc-header"><h1>Code and Comment</h1></div>
+}
+
+export default Header
