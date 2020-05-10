c87257d7b9d011e02f2746707382a4f092201c81
38elements <mh19820223@gmail.com>
2020-05-10 09:51:02 +0900

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
 
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
@@ -33,6 +34,7 @@ export default {
     }),
     commonjs({
       include: 'node_modules/**',
-    })
+    }),
+    replace({'process.env.NODE_ENV': 'production'})
   ]
 };
